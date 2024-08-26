3d49a035fa33ab7966c72ae7af497409e41bdb57
Hazumi Ichijo <hahihu314+github@gmail.com>
2024-08-27 01:14:47 +0900

    Fix diff

--- example/multiple_package/server/protos/one/one_pb.rb
+++ example/multiple_package/server/protos/one/one_pb.rb
@@ -3,20 +3,10 @@
 
 require 'google/protobuf'
 
-Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_file("protos/one/one.proto", :syntax => :proto3) do
-    add_message "example.multiple_package.protos.one.GiroTestRequest1" do
-      optional :message, :string, 1
-    end
-    add_message "example.multiple_package.protos.one.GiroTestRequest2" do
-    end
-    add_message "example.multiple_package.protos.one.GiroTestResponse1" do
-      optional :message, :string, 1
-    end
-    add_message "example.multiple_package.protos.one.GiroTestResponse2" do
-    end
-  end
-end
+descriptor_data = "\n\x14protos/one/one.proto\x12#example.multiple_package.protos.one\"#\n\x10GiroTestRequest1\x12\x0f\n\x07message\x18\x01 \x01(\t\"\x12\n\x10GiroTestRequest2\"$\n\x11GiroTestResponse1\x12\x0f\n\x07message\x18\x01 \x01(\t\"\x13\n\x11GiroTestResponse22\x89\x02\n\x0bGiroService\x12|\n\tGiroTest1\x12\x35.example.multiple_package.protos.one.GiroTestRequest1\x1a\x36.example.multiple_package.protos.one.GiroTestResponse1\"\x00\x12|\n\tGiroTest2\x12\x35.example.multiple_package.protos.one.GiroTestRequest2\x1a\x36.example.multiple_package.protos.one.GiroTestResponse2\"\x00\x42\x43ZAgithub.com/rerost/giro/example/multiple_package/protos/one;one_pbb\x06proto3"
+
+pool = Google::Protobuf::DescriptorPool.generated_pool
+pool.add_serialized_file(descriptor_data)
 
 module Example
   module MultiplePackage
